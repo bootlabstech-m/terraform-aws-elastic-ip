7183fbdbb65b267102707c39a04c7804202ea96c
Silpashree Acharya <silpashree@bootlabstech.com>
2023-08-02 14:55:55 +0530

    added lifecycle block

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 resource "aws_eip" "required-eip" {
   domain        = "vpc"
+    lifecycle {
+    ignore_changes = [tags]
+  }
 }
 resource "aws_eip_association" "required-eip-association" {
   instance_id   = var.instance_id
   allocation_id = aws_eip.required-eip.id
+
 }
